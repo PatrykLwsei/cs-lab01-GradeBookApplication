0129eafa94fab45ccc161257987e1808ed868164
PatrykLwsei <115666451+PatrykLwsei@users.noreply.github.com>
2023-04-04 20:21:26 +0200

    Calculate Student Statistics Added
    - [ ] Override `RankedGradeBook`'s `CalculateStudentStatistics` method
    		- [ ] Short circuit the method if there are less than 5 students.
    			- If there are less than 5 students write "Ranked grading requires at least 5 students." to the Console.
    			- If there are 5 or more students call the base class's `CalculateStudentStatistics` method using `base.CalculateStudentStatistics`.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,16 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
